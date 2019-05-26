e6733e0d0dc96cc952e19f47f9fc8255dd0dea24
robertd <robert.djurasaj@gmail.com>
2019-05-26 10:08:01 -0600

    Add geolambda example

--- lib/geolambda-python-example-stack.ts
+++ lib/geolambda-python-example-stack.ts
@@ -1,9 +1,26 @@
 import cdk = require('@aws-cdk/cdk');
+import lambda = require('@aws-cdk/aws-lambda');
 
 export class GeolambdaPythonExampleStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const geolambdaLayer = lambda.LayerVersion.fromLayerVersionArn(this, "geolambdaLayer", "arn:aws:lambda:us-west-2:552188055668:layer:geolambda:1");
+
+    const geolambdaPythonLayer = new lambda.LayerVersion(this, "geolambdaPythonLayer", {
+      name: "geolambdaPythonLayer",
+      description: "Geolambda Python bindings",
+      code: lambda.Code.asset("layer/geolambda-python.zip"),
+      compatibleRuntimes: [ lambda.Runtime.Python36, lambda.Runtime.Python37 ]
+    });
+
+    new lambda.Function(this, "CallGeolambdaPython", {
+      runtime: lambda.Runtime.Python36,
+      layers: [ geolambdaLayer, geolambdaPythonLayer ], 
+      code: lambda.Code.asset("lambda/python"),
+      handler: "lambda_function.lambda_handler",
+      timeout: 300,
+      description: "Lambda function that will invoke Geolambda Python bindings with GDAL"
+    });
   }
 }
