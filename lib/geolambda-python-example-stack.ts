5a3f538718ab00eb0c027fd0ce3102649519b5bd
robertd <robert.djurasaj@gmail.com>
2019-05-28 13:11:26 -0600

    Update example with custom geolambda and geolambda-python based on Python 3.7.3

--- lib/geolambda-python-example-stack.ts
+++ lib/geolambda-python-example-stack.ts
@@ -5,7 +5,14 @@ export class GeolambdaPythonExampleStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const geolambdaLayer = lambda.LayerVersion.fromLayerVersionArn(this, "geolambdaLayer", "arn:aws:lambda:us-west-2:552188055668:layer:geolambda:1");
+    //const geolambdaLayer = lambda.LayerVersion.fromLayerVersionArn(this, "geolambdaLayer", "arn:aws:lambda:us-west-2:552188055668:layer:geolambda:1");
+    
+    const geolambdaLayer = new lambda.LayerVersion(this, "geolambdaLayer", {
+      name: "geolambdaLayer",
+      description: "Geolambda layer",
+      code: lambda.Code.asset("layer/geolambda.zip"),
+      compatibleRuntimes: [ lambda.Runtime.Python36, lambda.Runtime.Python37 ]
+    });
 
     const geolambdaPythonLayer = new lambda.LayerVersion(this, "geolambdaPythonLayer", {
       name: "geolambdaPythonLayer",
@@ -15,7 +22,7 @@ export class GeolambdaPythonExampleStack extends cdk.Stack {
     });
 
     new lambda.Function(this, "CallGeolambdaPython", {
-      runtime: lambda.Runtime.Python36,
+      runtime: lambda.Runtime.Python37,
       layers: [ geolambdaLayer, geolambdaPythonLayer ], 
       code: lambda.Code.asset("lambda/python"),
       handler: "lambda_function.lambda_handler",
